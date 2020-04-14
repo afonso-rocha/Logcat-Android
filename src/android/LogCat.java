cb6b5150798ac80962ff6c8af436890caa251529
TotoHugo <42731864+TotoHugo@users.noreply.github.com>
2020-04-14 11:42:55 +0700

    Update LogCat.java

--- src/android/LogCat.java
+++ src/android/LogCat.java
@@ -17,7 +17,7 @@ public class LogCat extends CordovaPlugin {
 	    if (action.equals("sendLogs")) {
                         // save logcat in file
 		    //System.out.println(Environment.getDataDirectory());
-                File outputFile = new File(Environment.getFilesDir()
+                File outputFile = new File(Environment.getFilesDir(),
                         "logcat.txt");
                 try {
                     Runtime.getRuntime().exec(
