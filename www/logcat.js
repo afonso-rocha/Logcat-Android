4805fbd83eb89371449819ece1d8f2e73b3af0b5
TotoHugo <42731864+TotoHugo@users.noreply.github.com>
2020-04-14 12:21:24 +0700

    Update logcat.js

--- www/logcat.js
+++ www/logcat.js
@@ -1,6 +1,11 @@
-module.exports = {
+/*module.exports = {
 	sendLogs:function(successCB,failureCB){
 		cordova.exec(successCB(), failureCB(), "LogCat","sendLogs", []);
 	}
-};
+};*/
 
+var exec = require('cordova/exec');
+
+exports.sendLogs = function (arg0, success, error) {
+    exec(success, error, 'LogCat', 'sendLogs', [arg0]);
+};
