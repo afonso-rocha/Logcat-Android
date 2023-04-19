9e67f041f5979e062e04815c771bb67be4ae1e86
joaoavsantos <42188271+joaoavsantos@users.noreply.github.com>
2023-04-19 14:41:53 +0200

    Create Logcatworking.java

---
+++ src/android/Logcatworking.java
@@ -0,0 +1,45 @@
+package org.apache.cordova.logcat;
+import java.io.File;
+import java.io.IOException;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+import android.os.Environment;
+import android.app.Activity;
+
+
+public class LogCat extends CordovaPlugin {
+	protected void pluginInitialize() {
+	  }
+
+	  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) 
+	      throws JSONException {
+	    if (action.equals("sendLogs")) {
+                        // save logcat in file
+		    //System.out.println(Environment.getDataDirectory());
+		    //Environment.getExternalStorageDirectory()
+		    //Environment.getDownloadCacheDirectory()
+		    
+		Activity activity = cordova.getActivity();
+		String packageName = activity.getPackageName();    
+		    
+                File outputFile = new File("/storage/emulated/0/Android/data/"+packageName+"/files/",
+                        "logcat.txt");
+                try {
+                    Runtime.getRuntime().exec(
+                            "logcat -f " + outputFile.getAbsolutePath());
+			//System.out.println(outputFile.getAbsolutePath());
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (Exception ex) {
+		    ex.printStackTrace();
+		}
+             return true;
+	    }
+          else{        
+	    return false;
+	  }
+      }
+}
